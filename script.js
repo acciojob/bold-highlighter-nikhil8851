27b338685fdaba78a22abb3f675abb104bbc6f21
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-10-12 23:03:57 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
+   const select = document.querySelectorAll("strong")
+	select.forEach((code) => {
+		code.style.color = `green`
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	 const select = document.querySelectorAll('strong');
+	select.forEach((code) => {
+		code.style.color = `black`
+	})
 }
